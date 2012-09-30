33e5532dd1d33175058dbf65dcf6b739646dca39
luis <luxdao.scal@gmail.com>
2012-09-30 01:24:50 -0400

    envio

---
+++ seminarioblog/seminarioblog/Controllers/contenidoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using seminarioblog.Models;
+
+namespace seminarioblog.Controllers
+{
+    public class contenidoController : Controller
+    {
+        //
+        // GET: /contenido/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            conectorDataContext db = new conectorDataContext();
+            ViewBag.id = db.usuario.OrderByDescending(a => a.id).First().id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registrar(contenido content, string idUs)
+        {
+            if (ModelState.IsValid)
+            {
+                conectorDataContext db = new conectorDataContext();
+                content.fecha = DateTime.Now;
+                content.idUs = Convert.ToInt32(idUs);
+                db.contenido.InsertOnSubmit(content);
+                return RedirectToAction("exito", "contenido");
+            }
+            return View();
+        }
+    }
+}
---
+++ seminarioblog/seminarioblog/Controllers/usuarioController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using seminarioblog.Models;
+
+namespace seminarioblog.Controllers
+{
+    public class usuarioController : Controller
+    {
+        //
+        // GET: /usuario/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registrar(usuario newuser, string repass)
+        {
+            if (ModelState.IsValid)
+            {
+                conectorDataContext db = new conectorDataContext();
+                newuser.fecha = DateTime.Now;
+                db.usuario.InsertOnSubmit(newuser);
+                db.SubmitChanges();
+                return RedirectToAction("exito", "usuario");
+            }
+            return View();
+        }
+        public ActionResult exito()
+        {
+            return View();
+        }
+    }
+}
